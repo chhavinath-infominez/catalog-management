cd6a257e2cf16283636c0e0ad89d34a6ab2613df
Chhavinath Sahu <chhavinath@infominez.com>
2025-01-15 18:03:40 +0530

    Initial commit

---
+++ notification-service/src/test/java/com/infominez/catalog/notification/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infominez.catalog.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ payment-service/src/test/java/com/infominez/catalog/payment/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infominez.catalog.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
